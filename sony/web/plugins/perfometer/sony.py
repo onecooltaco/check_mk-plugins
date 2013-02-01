9f9bf47d3660a1135d257ba36fc8914fc7ca3036
Jeremy Leggat <jleggat@asu.edu>
2013-01-31 20:32:22 -0700

    projector power.

--- sony/web/plugins/perfometer/sony.py
+++ sony/web/plugins/perfometer/sony.py
@@ -3,13 +3,30 @@ def perfometer_check_mk_lamp(row, check_command, perf_data):
     crit = float(perf_data[0][4])
     warn = crit*0.85
     if left < warn:
-        color = "#00ff00"
+        color = "#008000"
     elif left < crit:
-        color = "#ffff00"
+        color = "#FFFF00"
     else:
-        color = "#ff0000"
+        color = "#FF0000"
     perc = (left / crit) * 100
     return "%.0f%%" % perc, perfometer_linear(perc, color)
 
 perfometers["check_mk-sony_vpl_lamp"] = perfometer_check_mk_lamp
 perfometers["check_mk-sony_srx_lamp"] = perfometer_check_mk_lamp
+
+def perfometer_check_mk_sony_power(row, check_command, perf_data):
+    state = float(perf_data[0][1])
+    statetxt = row["service_plugin_output"]
+
+    if state == 0:
+        color = "#FFFFFF"
+    elif state == 3:
+        color = "#00FF00"
+    elif state == 4:
+        color = "#00FFFF"
+    else:
+        color = "#FFA500"
+
+    return "%s" % statetxt, perfometer_linear(100, color)
+
+perfometers["check_mk-sony_vpl_power"] = perfometer_check_mk_sony_power
