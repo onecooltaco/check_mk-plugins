0755aed9142392e3030756ef712f6956262b0f8b
Jeremy Leggat <jleggat@asu.edu>
2013-11-07 16:00:32 -0700

    added xsan plugin to parse cvlog

--- xsan/web/plugins/perfometer/xsan.py
+++ xsan/web/plugins/perfometer/xsan.py
@@ -1,5 +1,11 @@
 perfometers["check_mk-xsan_stat.df"] = perfometer_check_mk_df
 
+def perfometer_check_mk_xsan_stat_info(row, check_command, perf_data):
+	color = { 0: "#6fc", 1: "#ffc", 2: "#f66", 3: "#fc0" }[row["service_state"]]
+	return "%d" % int(perf_data[0][1]), perfometer_logarithmic(perf_data[0][1], 20, 2, color)
+
+perfometers["check_mk-xsan_stat.info"] = perfometer_check_mk_xsan_stat_info
+
 def perfometer_check_mk_xsan_lag(row, check_command, perf_data):
     lag = float(perf_data[0][1])
     return "%d" % int(lag), perfometer_logarithmic(lag, 2000, 1.5, "#88c")
